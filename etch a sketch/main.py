e8e6aa25af67739e959616b00dc97759df0a8530
Herman Luo <hjl20@sfu.ca>
2022-11-06 11:48:37 -0800

    Add wasd, c key functions

--- etch a sketch/main.py
+++ etch a sketch/main.py
@@ -4,10 +4,31 @@ tim = Turtle()
 screen = Screen()
 
 
-def move_forwards():
+def move_forward():
     tim.forward(10)
 
 
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def reset_canvas():
+    tim.reset()
+
+
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(reset_canvas, "c")
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
 screen.exitonclick()
